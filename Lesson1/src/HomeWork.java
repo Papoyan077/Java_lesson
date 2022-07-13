ad625578600e0d91e40ca0bdaf97ee0d311f4d30
Papoyan077 <david.papoyan.1996@mail.com>
2022-07-13 17:38:20 +0400

    My First Commit

---
+++ Lesson1/src/HomeWork.java
@@ -0,0 +1,16 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        int birt=26;
+        byte age=26;
+        long day=2;
+        short mounth=2;
+        float born=1996f;
+        String name="David";
+        String surNAme="Papoyan";
+
+        System.out.println(
+                "name-"+name+"\n" + "surname-"+surNAme +"\n"+ "age-"+age+'\n'+"born-"+born+day+'.'+mounth
+        );
+
+    }
+}
---
+++ Lesson1/src/hello.java
@@ -0,0 +1,39 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.print("hey\n");
+        System.out.println("lesson 1");
+
+
+        byte x=127;
+        System.out.println(x);
+
+        short y = 4546;
+        System.out.println(y);
+
+        int z = 8798798;
+        System.out.println(z);
+
+        long l = 6542654565L;
+        System.out.println(l);
+
+        float f = 55.5f;
+        System.out.println(f);
+
+        double d =55.8;
+        System.out.println(d);
+
+        boolean is=false;
+        System.out.println(is);
+
+        is=true;
+        System.out.println(is);
+
+        char s = '\u003f';
+        System.out.println(s);
+
+        String firstName = "David";
+        String lastName = "Papoyan";
+        System.out.println(firstName+' '+lastName);
+
+    }
+}
